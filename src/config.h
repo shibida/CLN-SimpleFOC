414b236275de5b97c1f6a0c729b5fe90b4bdc39b
Yannick Peyrotau <yannick.peyrotau@csgroup.eu>
2025-11-04 21:02:06 +0100

    Configuration of my motor 42SHD0404-22

--- src/config.h
+++ src/config.h
@@ -7,14 +7,14 @@
 
 // Power supply
 
-#define POWER_SUPPLY_VOLTAGE 12.0f // [V]
+#define POWER_SUPPLY_VOLTAGE 45.0f // [V]
 
-// Motor
+// Motor 42SHD0404-22
 
 #define MOTOR_POLE_PAIR_NUMBER      50
-#define MOTOR_PHASE_RESISTANCE      1.3f    // [Ohms]
+#define MOTOR_PHASE_RESISTANCE      2.3f    // [Ohms]
 #define MOTOR_KV_RATING             NOT_SET // [RPM/V] (RMS value)
-#define MOTOR_PHASE_INDUCTANCE      0.0024f // [H]
+#define MOTOR_PHASE_INDUCTANCE      0.0028f // [H]
 #define MOTOR_SENSOR_ALIGN_VOLTAGE  5.0f    // [V]
 
 // Encoder
